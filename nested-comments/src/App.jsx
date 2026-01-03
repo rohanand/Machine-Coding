b8ed258f93e4aeff246981a72b072e694263b18c
Rohan Anand <rohan.anand1998@gmail.com>
2026-01-03 18:39:12 +0530

    Machine coding on Nested comments

---
+++ nested-comments/src/App.jsx
@@ -0,0 +1,12 @@
+import Comments from "./component/Comments";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Nested Comments</h2>
+      <Comments />
+    </div>
+  );
+}
+
+export default App;
